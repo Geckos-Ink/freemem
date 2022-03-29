6129adf16fd9c8537155b7f15598ff6b0be71985
Riccardo Cecchini <rcecchini.ds@gmail.com>
2022-03-29 23:47:53 +0200

    uff 😞
    19 hours later...

---
+++ src/database.js
@@ -0,0 +1,23 @@
+
+// All things she said, all about SQL
+exports = {};
+
+class Database {
+
+}
+
+exports.Database = Database;
+
+
+class Table {
+
+}
+
+exports.Table = Table;
+
+
+class Field {
+
+}
+
+exports.Field = Field;
--- src/var.js
+++ src/var.js
@@ -1,6 +1,20 @@
 
 class Var {
 
+    constructor(conn, path, parent = undefined){
+        this.conn = conn;
+        this.path = path;
+        this.parent = parent;
+    }
+
+    get(name){
+
+    }
+
+    set(val, name){
+
+    }
+
     proxy(){
         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy#browser_compatibility
         new Proxy(target, {

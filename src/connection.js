7e2ed685ede16133c4c79a381905aceaf2a8e34f
Riccardo Cecchini <rcecchini.ds@gmail.com>
2022-03-29 04:46:21 +0200

    somethin

--- src/connection.js
+++ src/connection.js
@@ -1,56 +1,68 @@
 
-exports = {};
-
-class Connection{
+// yup, this seems pretty weird
+exports = function Connections(){
 
 }
 
+class Connection {
+    constructor(opts){
+        opts = opts || {};
+
+        if(opts.host)
+            return new MySQL(opts);
+
+        if(!opts.file)
+            opts.file = ':memory:';
+        return new SQLite(opts);
+    }
+}
+
 class MySQL {
-    constructor(){
-        var mysql      = require('mysql');
-        var connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'me',
-        password : 'secret',
-        database : 'my_db'
+    constructor(opts){
+        var mysql = require('mysql');
+        this.connection = mysql.createConnection({
+        host     : opts.host,
+        user     : opts.user,
+        password : opts.pass,
+        database : opts.db
         });
         
-        connection.connect();
+        this.connection.connect();
         
-        connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+        this.connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
         if (error) throw error;
         console.log('The solution is: ', results[0].solution);
         });
         
-        connection.end();
+        this.connection.end();
 
         console.warn("MySQL to be implemented!");
     }
 }
 
-exports.MySQL = MySQL;
+Connection.MySQL = MySQL;
 
 class SQLite {
-    constructor(){
+    constructor(opts){
         var sqlite3 = require('sqlite3').verbose();
-        var db = new sqlite3.Database(':memory:');
+        this.db = new sqlite3.Database(opts.file); 
 
-        db.serialize(function() {
-        db.run("CREATE TABLE lorem (info TEXT)");
+        this.db.serialize(function() {
+            this.db.run("CREATE TABLE lorem (info TEXT)");
 
-        var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-        for (var i = 0; i < 10; i++) {
-            stmt.run("Ipsum " + i);
-        }
-        stmt.finalize();
+            var stmt = this.db.prepare("INSERT INTO lorem VALUES (?)");
+            for (var i = 0; i < 10; i++) {
+                stmt.run("Ipsum " + i);
+            }
+            stmt.finalize();
 
-        db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-            console.log(row.id + ": " + row.info);
-        });
+            this.db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+                console.log(row.id + ": " + row.info);
+            });
         });
 
         db.close();
     }
 }
 
-exports.SQLite = SQLite;
+Connection.SQLite = SQLite;

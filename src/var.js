0dbff48f8f8b85254b4fd1d04df43ac8ed3aeb84
Riccardo Cecchini <rcecchini.ds@gmail.com>
2022-03-29 06:00:57 +0200

    Create var.js

---
+++ src/var.js
@@ -0,0 +1,12 @@
+
+class Var {
+
+    proxy(){
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy#browser_compatibility
+        new Proxy(target, {
+            
+        });
+    }
+}
+
+exports = Var;
